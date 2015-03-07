01993216d8761d692632825cce27c388ce0eb467
Garrison Jensen <garrison.jensen@gmail.com>
2015-03-07 15:20:25 -0800

    First Yesod application playground.

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -12,70 +12,39 @@ import Import
 -- The majority of the code you will write in Yesod lives in these handler
 -- functions. You can spread them across multiple files if you are so
 -- inclined, or create a single monolithic file.
---
---
--- data FormResult a = FormMissing
---                   | FormFailure [Text]
---                   | FormSuccess a
---     deriving Show
--- instance Functor FormResult where
---     fmap _ FormMissing = FormMissing
---     fmap _ (FormFailure errs) = FormFailure errs
---     fmap f (FormSuccess a) = FormSuccess $ f a
--- instance Applicative FormResult where
---     pure = FormSuccess
---     (FormSuccess f) <*> (FormSuccess g) = FormSuccess $ f g
---     (FormFailure x) <*> (FormFailure y) = FormFailure $ x ++ y
---     (FormFailure x) <*> _ = FormFailure x
---     _ <*> (FormFailure y) = FormFailure y
---     _ <*> _ = FormMissing
--- instance Monoid m => Monoid (FormResult m) where
---     mempty = pure mempty
---     mappend x y = mappend <$> x <*> y
--- instance Semigroup m => Semigroup (FormResult m) where
---     x <> y = (<>) <$> x <*> y
 
 getHomeR :: Handler Html
 getHomeR = do
-    -- mmsg <- getMessage
     messages <- runDB $ selectList [] [Desc MessageTimestamp]
     mauth <- maybeAuth
-    (formWidget, formEnctype) <- generateFormPost messageForm
     let submission = Nothing :: Maybe Message
         handlerName = "getHomeR" :: Text
-    defaultLayout $ do
-        aDomId <- newIdent
-        setTitle "Just text"
-        $(widgetFile "homepage")
+    case mauth of
+      Just (Entity uid _) -> do
+        (formWidget, formEnctype) <- generateFormPost $ messageForm uid
+        defaultLayout $ do
+            aDomId <- newIdent
+            setTitle "Just text"
+            $(widgetFile "homepage")
+      Nothing -> defaultLayout $ do
+            aDomId <- newIdent
+            setTitle "Just text"
+            $(widgetFile "homepageNoForm")
 
 postHomeR ::  Handler ()
 postHomeR = do
-    ((message, _), _) <- runFormPost messageForm
+    user <- requireAuthId
+    ((message, _), _) <- runFormPost $ messageForm user
     case message of 
         FormSuccess a -> do 
                     _ <- runDB $ insert a
                     setMessage "Message posted :)"
-        FormFailure t -> setMessage $ toHtml $ show t ++ "D:"
+        FormFailure t -> setMessage $ toHtml $ show t ++ " D:"
         _             -> setMessage "Error"
     redirect HomeR
     
--- now <- liftIO getCurrentTime
--- _ <- runDB $ insert $ Message (Textarea mes) now
--- mes <- runInputPost $ ireq textField "message"
---     mmsg <- getMessage
---     -- (uid, _) <- requireAuth
---     ((FormResult a0, xml0), Enctype) <- runFormPost "message"
---     now <- liftIO getCurrentTime
---     runDB $ insert $ Message 3 message now
---     setMessage "Message posted :)"
---     redirect getHomeR
-
-messageForm :: Form Message
-messageForm = renderDivs $ Message
+messageForm :: UserId -> Form Message
+messageForm user = renderDivs $ Message
     <$> areq textareaField "" Nothing
+    <*> pure user
     <*> lift (liftIO getCurrentTime)
-
--- Message
---     user UserId Eq In
---     content String
---     timestamp UTCTime Desc
---
+++ Handler/Profile.hs
@@ -0,0 +1,11 @@
+module Handler.Profile where
+
+import Import
+
+getProfileR :: UserId -> Handler Html
+getProfileR userId = do
+    messages <- runDB $ selectList [MessageUser ==. userId] [Desc MessageTimestamp]
+    defaultLayout $ do
+        aDomId <- newIdent
+        setTitle "Just text"
+        $(widgetFile "profile")
