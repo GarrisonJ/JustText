31378ba909b4b53bce609d51b61b9920f78b4fef
Garrison Jensen <garrison.jensen@gmail.com>
2015-11-01 12:10:22 -0800

    Navbar should hide nav links on mobile. Use Hamburger button.

--- Handler/Navbar.hs
+++ Handler/Navbar.hs
@@ -3,22 +3,36 @@ module Handler.Navbar where
 import Import
 
 navbar :: Maybe (Entity User) -> Widget
-navbar mauth = toWidget [hamlet|
+navbar mauth = do
+        addStylesheetRemote "https://fonts.googleapis.com/icon?family=Material+Icons"
+        toWidget [julius|
+          $(".button-collapse").sideNav();
+        |]
+        toWidget [hamlet|
           <div class="row">
             <nav>
               <div .nav-wrapper .grey .lighten-3>
                 <a href=@{HomeR} .brand-logo>&nbsp;&#9646;&#9646;&#9646;
-                <ul .right>
-                  $maybe (Entity id _) <- mauth
+                $maybe (Entity id _) <- mauth
+                  <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+                  <ul .right .hide-on-med-and-down>
+                      <li>
+                        <a href=@{ProfileR id}>Profile
+                      <li>
+                        <a href=@{SettingsR}>Settings
+                      <li>
+                        <a href=@{AuthR LogoutR}>Logout
+                  <ul .side-nav #mobile-demo>
+                      <li>
+                        <a href=@{ProfileR id}>Profile
+                      <li>
+                        <a href=@{SettingsR}>Settings
+                      <li>
+                        <a href=@{AuthR LogoutR}>Logout
+                $nothing
+                  <ul .right>
                     <li>
-                      <a href=@{ProfileR id}>Profile
-                    <li>
-                      <a href=@{SettingsR}>Settings
-                    <li>
-                      <a href=@{AuthR LogoutR}>Logout
-                  $nothing
-                    <li>
-                      <a href=@{LandingR}><i>Login</i>
+                      <a href=@{LandingR} >Login
          |]
 
 
